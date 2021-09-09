8b47a63f00db6ae267b37d5d82d36c36a0b62ad9
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-09-09 15:38:10 -0500

    finished design

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_KEY = 'OYrC1WZg8vIJ3RwcdxfXPNaR8DKKNijv5qfsxgqt';
+const URL = 'https://api.nasa.gov/planetary/apod?api_key=OYrC1WZg8vIJ3RwcdxfXPNaR8DKKNijv5qfsxgqt';
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json');
